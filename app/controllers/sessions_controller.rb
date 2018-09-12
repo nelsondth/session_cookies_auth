e10a1799b49b5a2d954e4a75326b86c28c18fdca
Nelson Marcano <nelsonmarcano@devtohack.com>
2018-09-12 11:57:59 -0400

    manejo de sesiones

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,18 @@
 class SessionsController < ApplicationController
   def create
+    name = params[:name]
+    user = User.find_by(name: name)
+    if user
+      session[:user_id] = user.id
+      redirect_to books_path
+    else
+      render :new
+    end
   end
 
   def destroy
+    session.clear
+    redirect_to login_path
   end
 
   def new
