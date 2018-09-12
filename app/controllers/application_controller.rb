b3a5a187b75002604fb7188a13448a81fff76eb8
Nelson Marcano <nelsonmarcano@devtohack.com>
2018-09-12 12:10:14 -0400

    implementacion de current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
 
+  helper_method :current_user
 
 private
 
   def authenticate_user!
     redirect_to login_path unless session[:user_id]
+  end
+
+  def current_user
+    @user ||= User.find_by(id: session[:user_id])
   end
 
 end
